84a642f8ffd38d4bdfd3e0d1bd3e83340a678868
Plamen Popov <tzappa@gmail.com>
2024-12-11 10:25:35 +0200

    feat: implement routing and bootstrap files for API and Admin panel

---
+++ src/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Laminas\Diactoros\ServerRequestFactory;
+
+// Load Composer's autoloader
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$request = ServerRequestFactory::fromGlobals();
+
+$uri = $request->getUri()->getPath();
+if (str_starts_with($uri, '/adm')) {
+    require __DIR__ . '/Adm/bootstrap.php';
+} elseif (str_starts_with($uri, '/api')) {
+    require __DIR__ . '/API/bootstrap.php';
+} else {
+    require __DIR__ . '/App/bootstrap.php';
+}
