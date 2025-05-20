4ecd42bcec79e0f148aed3e2f58faa30ec74b8f7
Plamen Popov <tzappa@gmail.com>
2025-05-20 10:17:32 +0300

    fix: home test

--- tests/App/RequestHandler/HomeTest.php
+++ tests/App/RequestHandler/HomeTest.php
@@ -1,32 +1,63 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Tests\App\RequestHandler;
 
 use App\RequestHandler\Home;
-
-use Clear\Template\TwigTemplate;
-use Laminas\Diactoros\ServerRequest;
+use Clear\Template\TemplateInterface;
+use Laminas\Diactoros\Response\HtmlResponse;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 #[CoversClass(Home::class)]
-#[UsesClass(TwigTemplate::class)]
-final class HomeTest extends TestCase
+class HomeTest extends TestCase
 {
+    private MockObject $templateMock;
+    private MockObject $requestMock;
+    private Home $homeHandler;
+
+    protected function setUp(): void
+    {
+        $this->templateMock = $this->createMock(TemplateInterface::class);
+        $this->requestMock = $this->createMock(ServerRequestInterface::class);
+        $this->homeHandler = new Home($this->templateMock);
+    }
+
+    public function testImplementsRequestHandlerInterface(): void
+    {
+        $this->assertInstanceOf(RequestHandlerInterface::class, $this->homeHandler);
+    }
+
     public function testHandleReturnsHtmlResponse(): void
     {
-        $twig = new TwigTemplate(__DIR__ . '/../../../src/App/templates', false, false);
-        $twig->registerFunction('route', fn() => '/login');
-        $home = new Home($twig);
-        $request = new ServerRequest([], [], '/', 'GET');
+        $mockTemplate = $this->createMock(TemplateInterface::class);
+        $mockTemplate->expects($this->once())
+            ->method('load')
+            ->with('home.twig')
+            ->willReturn($mockTemplate);
 
-        $response = $home->handle($request);
+        $mockTemplate->expects($this->once())
+            ->method('parse')
+            ->willReturn('<html><body>Test Content</body></html>');
 
-        $this->assertInstanceOf(ResponseInterface::class, $response);
-        $this->assertSame(200, $response->getStatusCode());
-        $this->assertStringContainsString('<p>Welcome to PHPiko!</p>', (string) $response->getBody());
-        $this->assertStringContainsString('<a href="/login">Login</a>', (string) $response->getBody());
+        $homeHandler = new Home($mockTemplate);
+        $response = $homeHandler->handle($this->requestMock);
+
+        $this->assertInstanceOf(HtmlResponse::class, $response);
+        $this->assertEquals('<html><body>Test Content</body></html>', $response->getBody()->getContents());
+    }
+
+    public function testTemplateIsLoadedWithCorrectTemplate(): void
+    {
+        $this->templateMock->expects($this->once())
+            ->method('load')
+            ->with('home.twig');
+
+        $this->homeHandler->handle($this->requestMock);
     }
 }
